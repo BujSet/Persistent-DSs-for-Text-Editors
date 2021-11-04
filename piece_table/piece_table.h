8beb668327b1bc9c7a0c99d0f44de848c41eb7de
Ranganath Selagamsetty <selagamsetty@wisc.edu>
2021-11-04 17:36:29 -0500

    First commit

---
+++ piece_table/piece_table.h
@@ -0,0 +1,11 @@
+
+
+typedef struct piece_table_t {
+    int something_goes_here;
+} PT_t;
+
+/* Inserts character to piece table */
+void insert(PT_t *PT, char c);
+
+/* Deletes character from piece table */
+void delete(PT_t *PT);
