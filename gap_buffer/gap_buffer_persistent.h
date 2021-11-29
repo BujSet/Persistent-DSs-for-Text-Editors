8ceb9013b0e58b25aa0fd76f1934f431ad3d8f84
Chetan Shukla <chetanshukla95@gmail.com>
2021-11-29 00:23:13 -0600

    Initial layout of the persistent version of Gap Buffer

---
+++ gap_buffer/gap_buffer_persistent.h
@@ -0,0 +1,84 @@
+#include <unistd.h>
+#include <iostream>
+#include <libpmemobj.h>
+#include <libpmemobj++/transaction.hpp>
+#include <libpmemobj++/make_persistent.hpp>
+#include <libpmemobj++/p.hpp>
+#include <libpmemobj++/persistent_ptr.hpp>
+#include <libpmemobj++/pool.hpp>
+#include <libpmemobj++/container/string.hpp>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+namespace pobj = pmem::obj;
+
+#ifndef __GAP_BUFFER_H__
+#define __GAP_BUFFER_H__
+
+#define DEFAULT_LAYOUT "DEFAULT_LAYOUT"
+
+namespace GapBuffer {
+	enum SourceBuffer {ORIGINAL, ADD};
+	enum SeekDir {FWD=1, BWD=-1};
+
+	/*
+	using string_type = pobj::string;
+
+	typedef struct cursor {
+		cursor(int posarg) {
+			pos = posarg;
+		}
+
+		cursor(){			
+		}
+
+		pobj::p<size_t> pos;
+		pobj::p<size_t> piece_idx;
+		pobj::p<size_t> piece_offset;
+	} cursor;
+
+	typedef struct piece {
+		pobj::p<SourceBuffer> src;
+		pobj::p<size_t> start;
+		pobj::p<size_t> len;
+	} piece;
+	*/
+	using char_vector_type = pobj::vector<char>;
+
+	typedef struct gap_buffer {
+		pobj::p<size_t> gap_size;
+		pobj::p<size_t> gap_left;
+		pobj::p<size_t> gap_right;
+		pobj::p<size_t> size;
+		
+    	pobj::persistent_ptr<char_vector_type> buffer;
+
+	} gap_buffer;
+
+	typedef struct root {
+		pobj::persistent_ptr<gap_buffer> root_gap_buffer;
+	} root;
+
+
+	void create(pobj::pool<GapBuffer::root> pop, string file_path);
+
+	void insert(pobj::pool<GapBuffer::root> pop, string input, int position);
+
+	void deleteCharacter(pobj::pool<GapBuffer::root> pop, int position);
+
+	void moveCursor(pobj::pool<GapBuffer::root> pop, int position);
+
+	void left(pobj::pool<GapBuffer::root> pop, int position);
+
+	void right(pobj::pool<GapBuffer::root> pop, int position);
+
+	void grow(pobj::pool<GapBuffer::root> pop, int k, int position); 
+
+	void print_table(pobj::pool<GapBuffer::root> pop);
+
+	void close(pobj::pool<GapBuffer::root> pop, string file_path);
+}
+
+#endif /* __GAP_BUFFER_H__ */
