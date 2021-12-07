1a45b70952dac14565f6fa5203f58a360efcee3d
Chetan Shukla <chetanshukla95@gmail.com>
2021-12-07 12:48:47 -0600

    Fixing the comments

--- gap_buffer/gap_buffer_persistent.cpp
+++ gap_buffer/gap_buffer_persistent.cpp
@@ -214,7 +214,7 @@ void GapBuffer::right(pobj::pool<GapBuffer::root> pop, int position) {
     
     pobj::transaction::run(pop, [&] {
         // cout << "Inside right(), with position: " << position << " and gap_left: " 
-        << root_gap_buffer->gap_left << " and gap_right: " << root_gap_buffer->gap_right << endl;
+        // << root_gap_buffer->gap_left << " and gap_right: " << root_gap_buffer->gap_right << endl;
     
         // Moves the gap right, character by character
         while (position > root_gap_buffer->gap_left) {
